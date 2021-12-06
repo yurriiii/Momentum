704c179417badcc12d69fbbacd46aa298ac52adc
yurriiii <54986602+yurriiii@users.noreply.github.com>
2021-12-06 21:57:47 +0900

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,32 @@
+﻿const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log("hello");
+    const username = loginInput.value;
+    loginForm.classList.add("hidden");
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = "Hello " + username;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+} else {
+    //show the greeting
+    paintGreetings(savedUsername)
+}
---
+++ quotes.js
@@ -0,0 +1,50 @@
+﻿const quotes = [
+    {
+        quote:"I think that somehow, we learn who we really are and then live with that decision.",
+        author: "Eleanor Roosevelt",
+    },
+    {
+        quote:"Life is not fair; get used to it.",
+        author: "Bill Gates",
+    },
+    {
+        quote:"Politeness, n. The most acceptable hypocrisy.",
+        author: "Ambrose Bierce",
+    },
+    {
+        quote:"I hear and I forget. I see and I remember. I do and I understand.",
+        author: "Confucius",
+    },
+    {
+        quote:"A friend to all is a friend to none.",
+        author: "Aristotle",
+    },
+    {
+        quote:"Cherish all your happy moments: they make a fine cushion for old age.",
+        author: "Christopher Morley",
+    },
+    {
+        quote:"Study the past if you would define the future.",
+        author: "Confucius",
+    },
+    {
+        quote:"We need men who can dream of things that never were.",
+        author: "John F. Kennedy",
+    },
+    {
+        quote:"A room without books is like a body without a soul.",
+        author: "Cicero",
+    },
+    {
+        quote:"The man who moves a mountain begins by carrying away small stones.",
+        author: "Confucius",
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
